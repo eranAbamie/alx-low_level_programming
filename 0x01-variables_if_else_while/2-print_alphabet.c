343188aa48d79735aa901f5a69454669c2687e0e
eranAbamie <sirluwexi@gmail.com>
2022-03-12 23:26:11 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -15,3 +15,14 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+#include <stdlib.h>
+/**
+*main - prints the alphabet in lower case followed by a new line
+* Return : Always 0
+*/
+int main (void)
+{  char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar('\n');
+return (0);
+}
