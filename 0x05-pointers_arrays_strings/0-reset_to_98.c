5733920017548303025c6b96f2a9108f236cf02a
Ogunwale Emmanuel <sirluwexi@gmail.com>
2022-03-21 20:52:00 +0100

    task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ * takes a value and updates it to the value it points to
+ * @pointer
+ * Return: Always 0.
+ */
+int main void reset_to_98(int *n)
+{
+	*n = 98;
+}
