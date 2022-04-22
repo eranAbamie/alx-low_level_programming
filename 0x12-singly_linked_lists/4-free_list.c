459c31828c3626e20f2e30bee62daa61da9fdf77
Your Name <you@example.com>
2022-04-22 02:15:07 +0100

    task 2 and 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -17,3 +17,22 @@ void free_list(list_t *head)
 		free(current);
 	}
 }
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
